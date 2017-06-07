5c3a1438c4c8931ba94fc7ccdcca2808bfa91379
Lorenzo Tomasini <lorenzo.tomasini@gmail.com>
2017-06-07 12:09:26 +0200

    updated version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
 
 setup(
     name="openbaton-cli",
-    version="3.2.5",
+    version="3.2.6",
     author="Open Baton",
     author_email="dev@openbaton.org",
     description="The Open Baton CLI",
